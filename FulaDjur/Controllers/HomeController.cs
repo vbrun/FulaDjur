679abffabca934658a8310c693a5eb989c4ff1ec
vbrun <vbrunquaker@hotmail.com>
2014-11-28 12:20:40 +0100

    Lite testdata

--- FulaDjur/Controllers/HomeController.cs
+++ FulaDjur/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FulaDjur.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,14 @@ namespace FulaDjur.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var fuladjur = new List<FultDjur>()
+            {
+                new FultDjur { Rubrik = "Fulafisken", ImageUrl = "https://24tanzania.com/wp-content/uploads/2013/09/adeaBlobfish.jpg", UglyRating = 5 },
+                new FultDjur { Rubrik = "Könshår", ImageUrl = "http://www.oddee.com/_media/imgs/articles2/a97635_munchkin.jpg", UglyRating = 2 },
+                new FultDjur { Rubrik = "Uppklädd", ImageUrl = "http://littlefun.org/uploads/524b6b7be691b20cf6c4428c_736.jpg", UglyRating = 4 }
+            };
+
+            return View(fuladjur);
         }
 
         public ActionResult About()
---
+++ FulaDjur/Models/FultDjur.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FulaDjur.Models
+{
+    public class FultDjur
+    {
+        public string Rubrik { get; set; }
+        public string ImageUrl { get; set; }
+        public int UglyRating { get; set; }
+    }
+}
