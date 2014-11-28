8a5691e9e0b5e3e9ad196c8deee8d86bbdf40dfa
vbrun <vbrunquaker@hotmail.com>
2014-11-28 11:35:09 +0100

    Initial

---
+++ FulaDjur/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FulaDjur.Startup))]
+namespace FulaDjur
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
